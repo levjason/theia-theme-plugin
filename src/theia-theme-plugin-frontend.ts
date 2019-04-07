c6d74baf0bf22d041360d90b5bb2ab040c358908
Jason Levine <usajlev@usrscp29.eur.ad.sag>
2019-04-07 07:08:32 -0400

    changed font family

--- src/theia-theme-plugin-frontend.ts
+++ src/theia-theme-plugin-frontend.ts
@@ -8,11 +8,11 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css';
 
 export function start(context: theia.PluginContext) {
     console.log(theia.window.state);
-    document.documentElement.style.setProperty("--theia-ui-font-size1", "14px");
-    document.documentElement.style.setProperty("--theia-brand-color0", "#1776BF");
-    document.documentElement.style.setProperty("--theia-ui-font-family", "'Roboto', Arial, Sans-serif");
-    document.documentElement.style.setProperty("--theia-ui-font-color0", "#333333");
-    document.documentElement.style.setProperty("--theia-ui-font-color1", "#333333");
+    document.documentElement.style.setProperty("--theia-ui-font-size1","14px");
+    document.documentElement.style.setProperty("--theia-brand-color0","#1776BF");
+    document.documentElement.style.setProperty("--theia-ui-font-family","'Roboto', Courier, Sans-serif");
+    document.documentElement.style.setProperty("--theia-ui-font-color0","#333333");
+    document.documentElement.style.setProperty("--theia-ui-font-color1","#333333");
 }
 
 export function stop() {
